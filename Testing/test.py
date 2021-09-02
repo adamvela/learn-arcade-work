a0e8fb3ef6be3d4f26a02691e76e673796283aa9
Adam Velazquez <adam.velazquez@my.simpson.edu>
2021-09-02 15:19:20 -0500

    Lab 1

--- Testing/test.py
+++ Testing/test.py
@@ -1,8 +1,10 @@
-print("""Holy cow!
-The professor
-has
-gone
-crazy!
-""")
+print("The Talkings Heads are the best band of the genre, \"New Wave\"\n")
 
+print("This is going to be one of my favorite classes this semester\n")
 
+print("""Roses are red
+violets are blue
+coding is cool
+just like you\n""")
+
+print("I like my seat in this class, its right by the cold air conditioning")

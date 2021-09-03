6c681f8df92b22a9d13ab6d4f0777ff5f4678dd8
Adam Velazquez <adam.velazquez@my.simpson.edu>
2021-09-03 11:30:22 -0500

    Lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,11 +1,11 @@
-print("The Talkings Heads are the best band of the genre, \"New Wave\"\n")
+print("The Talking Heads are the best band of the genre, \"New Wave\".\n")
 
-print("This is going to be one of my favorite classes this semester\n")
+print("This is going to be one of my favorite classes this semester.\n")
 
-print("""Roses are red
-violets are blue
-coding is cool
-just like you\n""")
+print("""Roses are red,
+violets are blue,
+coding is cool,
+just like you.\n""")
 
-print("I like my seat in this class, its right by the cold air conditioning")
+print("I like my seat in this class, it's right by the cold air conditioning.")
 

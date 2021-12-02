dc95b456452d0b14f93834006c06fb4bb2d631d7
Adam Velazquez <adam.velazquez@my.simpson.edu>
2021-12-02 15:40:49 -0600

    Lab 12

--- Lab 12 - Final Lab/part_12.py
+++ Lab 12 - Final Lab/part_12.py
@@ -1,14 +1,15 @@
+""" Sprite Sample Program """
 
 import random
 import arcade
 
 # --- Constants ---
-SPRITE_SCALING_CRAVEN = 0.5
-SPRITE_SCALING_BEER = 0.2
-CRAVEN_MOVEMENT_SPEED = 5
+SPRITE_SCALING_PLAYER = 0.5
+SPRITE_SCALING_BEER = 0.035
+PLAYER_MOVEMENT_SPEED = 5
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
 
 
 class MyGame(arcade.Window):
@@ -17,15 +18,18 @@ class MyGame(arcade.Window):
     def __init__(self):
         """ Initializer """
         # Call the parent class initializer
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 12")
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Example")
 
         # Variables that will hold sprite lists
-        self.craven_list = None
+        self.player_list = None
         self.beer_list = None
 
         # Set up the player info
-        self.craven_sprite = None
+        self.player_sprite = None
         self.score = 0
+
+        # Don't show the mouse cursor
+        self.set_mouse_visible(False)
 
         arcade.set_background_color(arcade.color.AMAZON)
 
@@ -33,7 +37,7 @@ class MyGame(arcade.Window):
         """ Set up the game and initialize the variables. """
 
         # Sprite lists
-        self.craven_list = arcade.SpriteList()
+        self.player_list = arcade.SpriteList()
         self.beer_list = arcade.SpriteList()
 
         # Score
@@ -41,25 +45,26 @@ class MyGame(arcade.Window):
 
         # Set up the player
         # Character image from kenney.nl
-        self.craven_sprite = arcade.Sprite("character.png", SPRITE_SCALING_CRAVEN)
-        self.craven_sprite.center_x = 50
-        self.craven_sprite.center_y = 50
-        self.craven_list.append(self.craven_sprite)
+        self.player_sprite = arcade.Sprite("character.png", SPRITE_SCALING_PLAYER)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 50
+        self.player_list.append(self.player_sprite)
 
-        beer = arcade.Sprite("coin_01.png", SPRITE_SCALING_BEER)
+        # Beer image comes from heineken.com
+        beer = arcade.Sprite("heineken-original-bottle.png", SPRITE_SCALING_BEER)
 
-        # Position the coin
-        beer.center_x = random.randrange(SCREEN_WIDTH - 5)
-        beer.center_y = random.randrange(SCREEN_HEIGHT - 5)
+        # Position the beer
+        beer.center_x = random.randrange(SCREEN_WIDTH)
+        beer.center_y = random.randrange(SCREEN_HEIGHT)
 
-        # Add the coin to the lists
+        # Add the beer to the lists
         self.beer_list.append(beer)
 
     def on_draw(self):
         """ Draw everything """
         arcade.start_render()
         self.beer_list.draw()
-        self.craven_list.draw()
+        self.player_list.draw()
 
         # Put the text on the screen.
         output = f"Score: {self.score}"
@@ -68,38 +73,37 @@ class MyGame(arcade.Window):
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
         if key == arcade.key.UP:
-            self.craven_sprite.change_y = CRAVEN_MOVEMENT_SPEED
+            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
         elif key == arcade.key.DOWN:
-            self.craven_sprite.change_y = -CRAVEN_MOVEMENT_SPEED
+            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
         elif key == arcade.key.LEFT:
-            self.craven_sprite.change_x = -CRAVEN_MOVEMENT_SPEED
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
         elif key == arcade.key.RIGHT:
-            self.craven_sprite.change_x = CRAVEN_MOVEMENT_SPEED
-
-    def on_key_release(self, key, modifiers):
-        if key == arcade.key.UP or key == arcade.key.DOWN:
-            self.craven_sprite.change_y = 0
-        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.craven_sprite.change_x = 0
-        elif len(self.worm_list) > 0:
-            self.craven_sprite.center_x = 0
-            self.craven_sprite.center_y = 0
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
     def update(self, delta_time):
         """ Movement and game logic """
         # Call update on all sprites (The sprites don't do much in this
         # example though.)
         self.beer_list.update()
-
+        self.player_list.update()
         # Generate a list of all sprites that collided with the player.
-        beers_hit_list = arcade.check_for_collision_with_list(self.craven_sprite, self.beer_list)
+        beer_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.beer_list)
+
         # Loop through each colliding sprite, remove it, and add to the score.
-        for beer in beers_hit_list:
+        for beer in beer_hit_list:
             beer.remove_from_sprite_lists()
             self.score += 1
-            if beer in beers_hit_list == 0:
-                beer.center_x = random.randrange(SCREEN_WIDTH)
-                beer.center_y = random.randrange(SCREEN_HEIGHT)
+            # Coin image from kenney.nl
+            beer = arcade.Sprite("heineken-original-bottle.png", SPRITE_SCALING_BEER)
+
+            # Position the coin
+            beer.center_x = random.randrange(SCREEN_WIDTH)
+            beer.center_y = random.randrange(SCREEN_HEIGHT)
+
+            # Add the coin to the lists
+            self.beer_list.append(beer)
+            self.player_list.draw()
 
 
 def main():
